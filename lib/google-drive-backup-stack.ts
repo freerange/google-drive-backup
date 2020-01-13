932178c13f88b429b958ce9d8aa26a18dcb8c59e
James Mead <james@floehopper.org>
2020-01-13 19:23:44 +0000

    Use a custom VPC with a single availability zone
    This should reduce the number of availability zones from 2 to 1 and
    hence the number of NAT gateways from 2 to 1. This should reduce the
    costs of running the project.

--- lib/google-drive-backup-stack.ts
+++ lib/google-drive-backup-stack.ts
@@ -2,12 +2,17 @@ import cdk = require('@aws-cdk/core');
 import ecsPatterns = require('@aws-cdk/aws-ecs-patterns');
 import ecs = require('@aws-cdk/aws-ecs');
 import events = require('@aws-cdk/aws-events');
+import ec2 = require('@aws-cdk/aws-ec2');
 
 export class GoogleDriveBackupStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const vpc = new ec2.Vpc(this, 'vpc', { maxAzs: 1 });
+    const cluster = new ecs.Cluster(this, 'cluster', { vpc });
+
     const backupTask = new ecsPatterns.ScheduledFargateTask(this, 'backupTask', {
+      cluster: cluster,
       scheduledFargateTaskImageOptions: {
         image: ecs.ContainerImage.fromAsset('./local-image'),
         cpu: 4 * 1024,
